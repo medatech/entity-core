48cdc5d4f415df1ca30fd9a48a5d3bf64a627ba1
Martin Adams <martin@meda.co.uk>
2020-02-23 01:22:24 +0000

    Setting up code coverage

--- packages/pg/src/PostgresModel.test.ts
+++ packages/pg/src/PostgresModel.test.ts
@@ -1,5 +1,15 @@
-describe('PostgresModel', () => {
-    test('that something works', () => {
-        expect(1).toBe(1)
+import PostgresDataSource from './PostgresDataSource'
+
+describe('PostgresDataSource', () => {
+    test('should allow me to create a data source with a config', () => {
+        const config = {
+            host: 'localhost',
+            user: 'user',
+            password: 'dolphins',
+            database: 'sandbox'
+        }
+
+        const dataSource = new PostgresDataSource({ ...config })
+        expect(dataSource.poolConfig).toMatchObject(config)
     })
 })
