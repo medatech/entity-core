6ac047f2b2e04d2f387760df5c08ba0ee8448959
Martin Adams <martin.adams@meda.co.uk>
2020-10-26 21:54:09 +0000

    Exporting type definitions

--- packages/pg/src/index.ts
+++ packages/pg/src/index.ts
@@ -1,3 +1,4 @@
 export { default as PostgresDataSource } from "./PostgresDataSource"
 export { default as PostgresClient } from "./PostgresClient"
 export * from "./lib"
+export * from "./interfaces"
