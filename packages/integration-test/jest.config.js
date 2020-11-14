46f7592cee6ed76b3e4f61b947a83531a7196913
Martin Adams <martin@meda.co.uk>
2020-11-14 23:45:27 +0000

    Updating config for integration tests

--- packages/integration-test/jest.config.js
+++ packages/integration-test/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    collectCoverageFrom: ["src/**/*.{ts,js}"],
+    collectCoverageFrom: ["../pg/**/*.{ts,js}"],
     transform: {}
 }; 
